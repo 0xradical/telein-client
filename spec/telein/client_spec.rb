a18dfaacf708f4fc4a153888b6cf4151f66c5b75
Daniel Ribeiro Moreira <daniel@ribeiromoreira.com>
2014-05-27 15:59:54 -0300

    adds test for new functionality

--- spec/telein/client_spec.rb
+++ spec/telein/client_spec.rb
@@ -38,6 +38,19 @@ describe Telein::Client do
     client.carrier_code_for('(12) 9434-5656').should == 41
   end
 
+  it 'returns both carrier code and fixed number' do
+    Telein.servers.each do |server|
+      stub_request(:get,server.query_url_for('0000000000')).to_return(:body => '99#0000000000')
+      stub_request(:get,server.query_url_for('1234345656')).to_return(:body => '98#1234345656')
+      stub_request(:get,server.query_url_for('1294345656')).to_return(:body => '41#12994345656')
+    end
+
+    client = described_class.new
+    expect(client.query('(00) 0000-0000')).to  eq({ carrier_code: 100, number: "(00) 0000-0000"})
+    expect(client.query('(12) 3434-5656')).to  eq({ carrier_code:  98, number: "1234345656"})
+    expect(client.query('(12) 9434-5656')).to  eq({ carrier_code:  41, number: "12994345656"})
+  end
+
   it 'returns server error code when all endpoints are down' do
     Telein.servers.each do |server|
       stub_request(:get,server.query_url_for('0000000000')).to_timeout
